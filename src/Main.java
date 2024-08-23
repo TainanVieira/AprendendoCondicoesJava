9967532ef04cf4395a734e3916e1b67a1fe38de7
f-Brizola <brizola95@gmail.com>
2024-08-22 20:47:38 -0400

    Criação do Projeto Java.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+        Integer idadePessoa = 20;
+        String NomePessoa = "Tainã";
+
+
+        /*
+         *Operadores logicos
+         * >= maior igual
+         * <= Menor igual
+         * == è igual
+         * equal: compara textos
+         * != diferente
+
+         **/
+        if (idadePessoa > 21){
+            System.out.println("é maior que 21");
+        }else {
+            System.out.println("Não é maior");
+        }
 
 
 
