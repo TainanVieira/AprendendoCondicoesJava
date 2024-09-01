c95b952c5a9caaf1c7d0aafdbb9759ca7af851da
Tainan <tainanvieira21@gmail.com>
2024-09-01 19:05:31 -0300

    atividade
    Signed-off-by: Tainan <tainanvieira21@gmail.com>

---
+++ src/atividade_9_verificacao_de_numeros_positivo_negativo_ou_0.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class atividade_9_verificacao_de_numeros_positivo_negativo_ou_0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um Numero: ");
+        Double numero = scanner.nextDouble();
+
+        if ( numero > 0){
+            System.out.println("O Numero é Positivo.");
+        }
+        else if (numero < 0){
+            System.out.println(" O Numero é negativo.");
+        }
+        else if (numero == 0) {
+            System.out.println(" O Numero é Zero.");
+        }
+    }
+}
