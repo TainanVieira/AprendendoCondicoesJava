a9e01bd4c82b72232a632008524bafdebad4bcde
Tainan <tainanvieira21@gmail.com>
2024-09-01 19:05:04 -0300

    atividade
    Signed-off-by: Tainan <tainanvieira21@gmail.com>

---
+++ src/atividade8classificacaodetemperatura.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class atividade8classificacaodetemperatura {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite Qual a  Temperatura? ");
+        Integer temperatura = scanner.nextInt();
+
+        if (temperatura > 30){
+            System.out.println("A Temperatura está Quente.");
+        }
+        else if (temperatura >= 15 && temperatura <=30) {
+            System.out.println("A temperatura está Agradavel.");
+        }
+        else if (temperatura < 15) {
+            System.out.println("A Temperatura está Frio");
+        }
+
+
+    }
+}
